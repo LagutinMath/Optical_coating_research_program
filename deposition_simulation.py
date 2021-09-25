7e3ce45286640f18c0946c1f2cf97b0957c107da
LagutinMath <lagutin.math@gmail.com>
2021-09-25 15:30:44 +0300

    del convert tolist

--- deposition_simulation.py
+++ deposition_simulation.py
@@ -684,7 +684,7 @@ def collect_statistics(des_th, term_algs, set_up_pars, N_sim, start_rnd_seed=100
     for x in range(N_sim):
         rnd_seed = start_rnd_seed + x
         res = simulation(des_th, term_algs, set_up_pars, rnd_seed)
-        err_list[x] = res.errors_d[1:des_th.N + 1].tolist()
+        err_list[x] = res.errors_d[1:des_th.N + 1]
 
     return StatInfo(des_th, term_algs, set_up_pars, err_list, start_rnd_seed)
 
